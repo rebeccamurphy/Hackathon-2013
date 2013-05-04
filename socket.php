679f21637d101f787045234856967ad990487d4a
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2013-05-04 02:48:33 -0400

    socket.php now communicates with the server well enough. Up to Config. Needs cleaning. RESTART APACHE IF IT WONT DIE.

--- socket.php
+++ socket.php
@@ -1,5 +1,4 @@
 <?php
-#!/usr/bin/php5
 
 $sock = socket_create(AF_INET, SOCK_STREAM, 0);
 $ip_address = gethostbyname("hackathon.hopto.org");
@@ -14,7 +13,7 @@ if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
  
 echo "Socket created";
 
-if(!socket_connect($sock , '67.202.15.69' , 19012))
+if(!socket_connect($sock , '67.202.15.69' , 19013))
 {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
@@ -26,7 +25,7 @@ echo "Connection established \n";
 $message = "INIT Guffaws";
  
 //Send the message to the server
-if( ! socket_send ( $sock , $message , strlen($message) , 0))
+if( ! socket_send ( $sock , "INIT Guffaws", strlen($message) , 0))
 {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
@@ -37,7 +36,7 @@ if( ! socket_send ( $sock , $message , strlen($message) , 0))
 echo "Message send successfully \n";
  
 //Now receive reply from server
-if(socket_recv ( $sock , $buf , 2045 , MSG_PEEK ) === FALSE)
+if(socket_recv ( $sock , $buf , 6, MSG_WAITALL ) === FALSE)
 {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
@@ -45,9 +44,70 @@ if(socket_recv ( $sock , $buf , 2045 , MSG_PEEK ) === FALSE)
     die("Could not receive data: [$errorcode] $errormsg \n");
 }
 
+if( ! socket_send ( $sock , "RECD", strlen($message) , 0))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not send data: [$errorcode] $errormsg \n");
+}
+ 
+echo "Message send successfully \n";
+ 
+//Now receive reply from server
+if(socket_recv ( $sock , $buf , 14, MSG_WAITALL ) === FALSE)
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not receive data: [$errorcode] $errormsg \n");
+}
+ 
+//print the received message
+echo strlen($buf);
+	
+if( ! socket_send ( $sock , "START", strlen($message) , 0))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not send data: [$errorcode] $errormsg \n");
+}
+ 
+echo "Message send successfully \n";
+ 
+//Now receive reply from server
+if(socket_recv ( $sock , $buf , 24, MSG_WAITALL ) === FALSE)
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not receive data: [$errorcode] $errormsg \n");
+}
  
 //print the received message
 echo $buf;
-	
+
+if( ! socket_send ( $sock , "RECD", strlen($message) , 0))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not send data: [$errorcode] $errormsg \n");
+}
+ 
+echo "Message send successfully \n";
+ 
+//Now receive reply from server
+if(socket_recv ( $sock , $buf ,20, MSG_WAITALL ) === FALSE)
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not receive data: [$errorcode] $errormsg \n");
+}
+
+
+
 socket_close($sock);
 ?>
