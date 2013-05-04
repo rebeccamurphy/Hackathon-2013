613e607fe2d3b0bff50a83f84baf02550ab90631
John Paul Welsh <wearing2raincoats@yahoo.com>
2013-05-03 20:46:09 -0400

    winsock--> most of page 2
    closesocket, byte ordering, sockaddr

--- winsocktutorial.cpp
+++ winsocktutorial.cpp
@@ -5,7 +5,8 @@ Information Gleaned from the Windows Tutorial on Socket Programming (Winsock)
     - blocking: old way
         - "oh, you didn't successfully perform the operation you wanted? No API for you!"
     - non-blocking: new way
-        - "you didn't finish it, but my function will return as soon as possible, idgaf. I'll just send a message and it'll be k."
+        - "you didn't finish it, but my function will return as soon as possible, idgaf.
+           I'll just send a message and it'll be k."
 
 * Step-by-step
 1) initialize the winstock library
@@ -13,6 +14,23 @@ Information Gleaned from the Windows Tutorial on Socket Programming (Winsock)
                V-- highest version     V-- pointer to data struct that recieves details of implementation
 int WSAStartup(WORD wVersionRequested, LPWSADATA lpWSAData);
 int WSACleanup();
+
+2) set up socket
+
+3) close socket
+
+---Definition for Byte Ordering---
+Because protocols like TCP/IP have to work between different type of systems with different
+type of byte ordering, the standard is that values are stored in big-endian format, also
+called network byte order. For example, a port number (which is a 16-bit number) like 12345
+(0x3039) is stored with its most significant byte first (ie. first 0x30, then 0x39). A
+32-bit IP address is stored in the same way, each part of the IP number is stored in one byte,
+and the first part is stored in the first byte. For example, 216.239.51.100 is stored as the
+byte sequence '216,239,51,100', in that order.
+
+4) sockaddr
+
+5) 
 
 */
 
@@ -43,6 +61,7 @@ else {
 
 SOCKET hSocket;
 
+//       address family, type of socket, protocol
 hSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 if (hSocket==INVALID_SOCKET) {
@@ -51,10 +70,29 @@ if (hSocket==INVALID_SOCKET) {
 
 //=======================================================================
 
+closesocket(hSocket);
 
+//=======================================================================
 
+sockaddr_in sockAddr1, sockAddr2;
 
+// Set address family
+sockAddr1.sin_family = AF_INET;
 
+/* Convert port number 80 to network byte order and assign it to
+   the right structure member. */
+sockAddr1.sin_port = htons(80);
+
+/* inet_addr converts a string with an IP address in dotted format to
+   a long value which is the IP in network byte order.
+   sin_addr.S_un.S_addr specifies the long value in the address union */
+sockAddr1.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+// Set address of sockAddr2 by setting the 4 byte parts:
+sockAddr2.sin_addr.S_un.S_un_b.s_b1 = 127;
+sockAddr2.sin_addr.S_un.S_un_b.s_b2 = 0;
+sockAddr2.sin_addr.S_un.S_un_b.s_b3 = 0;
+sockAddr2.sin_addr.S_un.S_un_b.s_b4 = 1;
 
 
 
