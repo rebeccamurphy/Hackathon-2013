615a1d30cc8d31c0cf927a2185a40ef011f81c3b
Christina Ramos <galaxi1313@gmail.com>
2013-05-04 19:00:23 -0400

    Algorithm is beginning to come together.  No connection to php yet
    At least checks if a server is full and adds transactions to it if not.
    If it IS full, it will try to add the transactions to the next server in
    the same region.  If all the servers in the region are full (as in, at
    180, which means we lose no transactions), then we will fill up the
    servers as much as possible, and then calculate the left over
    transactions, and figure out what is profitable to do from there.

--- Algorithm/webserver.cpp
+++ Algorithm/webserver.cpp
@@ -1,36 +1,71 @@
-//Let's say we're deciding between transferring a client over to 
+//Let's say we're deciding between transferring a client over to
 //another server, or turning a server on in his most local region
 //Example: Tranferring client to EU from NA, or turning on NA server
 //on webserver tier
-//kill yourself pls
-	
 
-	
-	
+
 //let's assume that we got the information and it's parsed and stored
-//in variables of some sort?	
+//in variables of some sort?
+//((profitTrans * numTrans) + proportionSwitchEU(profitTrans*numTransFROMEU) + proportionSwitchAP(profitTrans*numTransFROMAP))proportionNumTrans
+//+ for each server in the region since the number of people is different... - serverCost * numServersNA
+//the repeat for eu or ap
 
-	
-	
-	
+//>
+
+//((profitTrans*numTrans) + proportionSwitchEU(profitTrans*numTransFROMEU) + proportionSwitchAP(profitTrans*numTransFROMAP))lossNumTrans
+// + for each server in the region since the number of people is different... - serverCost * (numServersNA + 1) THEN SAME THING ONLY NOT n+1
 class WebServer{
-	protected: 
-		int serverCost;
-		int numTrans;
-						
-		
+	protected:
+		int serverCost;											//cost of the server
+		int numTrans;											//number of transactions currently in the object
+
 	public:
 		WebServer(): serverCost(0), numTrans(0){}
 		WebServer(int cost, int numT){
-			serverCost = cost;
+			serverCost = cost;									//has to be tweaked to get info from game
 			numTrans = numT;
 		}
-		fillServer(int newTrans){
-			numTrans += newTrans;
+		void turnOn();											//this is entirely reliant on how we get info
+		void turnOff();											//and send it back to the game
+		bool isProfit(){
+
 		}
-		
-		{
-			
+		int fillServer(int newTrans){
+			i = 1
+			int leftOver;
+			if (server(i).numTrans + newTrans < 180) then{		//if the current transaction number + the transactions to be added are under 180 then
+				server(i).numTrans += newTrans;					//add the transactions to the server
+				newTrans = 0;
+				break;
+
+			}
+			else {
+				if (i != numServers) {	//if the current server is not the last one (doesn't equal the total number of servers)
+					for (j = i+1; j <=numServers; j++)		//starting at the next server
+					{
+						if (server(j).numTrans + newTrans < 180){	//if adding the transactions will not exceed 180
+							server(j).numTrans = server(j).numTrans + newTrans;		//then add them broski
+							newTrans = 0;
+							break;
+						}
+
+					}
+				}else{
+					for (i; i<=numServers; i++){
+
+						leftOver = server(i).numTrans + newTrans - 200;  //how many transfers left over
+						server(i).numTrans = server(i).numTrans + newTrans - leftOver;
+						newTrans = leftOver;
+
+						if (newTrans = 0) then{
+							break;
+						}
+					}
+
+					newTrans TRANSFERES ARE LEFT SO YOU MUST DO THE THING
+
+				}
+
+			}
 		}
-		
-		
+
