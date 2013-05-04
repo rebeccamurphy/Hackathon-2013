c326b60a8aee94923b3e848885e7cf7d190418df
John Paul Welsh <wearing2raincoats@yahoo.com>
2013-05-03 20:30:11 -0400

    information fro winsock tutorial, page 1-2
    only half of page 2

---
+++ winsocktutorial.cpp
@@ -0,0 +1,40 @@
+/*
+Information Gleaned from the Windows Tutorial on Socket Programming (Winsock)
+
+* The difference b/t blocking and non-blocking circuits
+    - blocking: old way
+        - "oh, you didn't successfully perform the operation you wanted? No API for you!"
+    - non-blocking: new way
+        - "you didn't finish it, but my function will return as soon as possible, idgaf. I'll just send a message and it'll be k."
+
+* Step-by-step
+1) initialize the winstock library
+
+               V-- highest version     V-- pointer to data struct that recieves details of implementation
+int WSAStartup(WORD wVersionRequested, LPWSADATA lpWSAData);
+int WSACleanup();
+
+*/
+
+const int iReqWinsockVer = 2;   // Minimum winsock version required
+
+WSADATA wsaData;
+
+if (WSAStartup(MAKEWORD(iReqWinsockVer,0), &wsaData)==0) {
+
+    // Check if major version is at least iReqWinsockVer
+    if (LOBYTE(wsaData.wVersion) >= iReqWinsockVer) {
+        /* ------- Call winsock functions here ------- */
+    }
+    else {
+        // Required version not available
+    }
+
+    // Cleanup winsock
+    if (WSACleanup()!=0) {
+        // cleanup failed
+    }
+}
+else {
+    //  startup failed
+}
