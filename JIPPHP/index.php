9f3f98c8c173d8d036018e8d6e421693dc302f6f
John Paul Welsh <wearing2raincoats@yahoo.com>
2013-05-05 14:17:33 -0400

    tiny changes, mostly deleting extra code

--- JIPPHP/index.php
+++ JIPPHP/index.php
@@ -27,7 +27,7 @@ $y_axis_range_step = 1000;
 
 $lineChart = new gLineChart(1000,600);
 $lineChart->addDataSet($data_arr);
-$lineChart->setLegend(array("Profits"));
+$lineChart->setLegend(array("Profit per Turn"));
 $lineChart->setColors(array("250066")); //deep blue
 $lineChart->setVisibleAxes(array('x','y')); //I think the only valid values for this are 'x' or 'y'
 $lineChart->setDataRange($y_axis_range_min, $y_axis_range_max); //I think this counts for both axes
@@ -35,27 +35,11 @@ $lineChart->setDataRange($y_axis_range_min, $y_axis_range_max); //I think this c
 $lineChart->addAxisRange(0, $x_axis_range_min, $x_axis_range_max, $x_axis_range_step);
 $lineChart->addAxisRange(1, $y_axis_range_min, $y_axis_range_max, $y_axis_range_step);
 $lineChart->addAxisLabel(0, array("Turn"));
-$lineChart->addAxisLabel(1, array("Profits"));
+$lineChart->addAxisLabel(1, array("Moneys"));
 //PARAMETERS below: part of chart being filled, color
 $lineChart->addBackgroundFill('bg', 'a21044'); //purple-ish red
 $lineChart->addBackgroundFill('c', '000000');  //black
 ?>
 <img src="<?php print $lineChart->getUrl();  ?>" /> <br> line chart using the gLineChart class.
-
-<!---
-<h1>PHP Wrapper for Google Chart API - Serverside Rendering - 0.1</h1>
-<h3>URL Request</h3>
-For the image below, post=false
-<p>
-<img src="img.php?width=300&amp;height=200&amp;post=false" />
-</p>
-<p>
-<h3>Post request</h3>
-For the image below, post=true
-<p>
-<img src="img.php?width=300&amp;height=200&amp;post=true" />
-</p>
--->
-
 </body>
 </html>
