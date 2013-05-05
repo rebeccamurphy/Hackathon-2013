46366fa75a4aa0d0f06962c20fd0174fee27c9d0
Christina Ramos <galaxi1313@gmail.com>
2013-05-05 12:58:31 -0400

    Cheap as balls algorithm returns an array of ints that will be converted into a string to give a command to the game
    Simply checks if there are 1 or less servers in a location.  If there
    are, it will add one.  If there are any more than 1 server(s), it will
    remove 1.

--- Algorithm/Cheap Algorithm.cpp
+++ Algorithm/Cheap Algorithm.cpp
@@ -1,39 +1,31 @@
-
-//revenue, wcost, jcost, dcost: [9,3,6,15]
-
-//wNA, wEU, wAP, jNA, jEU, jAP, dNA, dEU, dAP: [2,2,2,1,1,1,0,1,0]
-
-//day, hh, mm, ss, numTransNA, numTransEU, numTransAP: [MON,0,0,0,176,248,621]
-
-//revTrans numTrans 
-
 #include <iostream>
+#include <string>
+using namespace std;
 int main(){
+    /*
+    int revenue;
+    int wcost;
+    int jcost;
+    int dcost;
+    int numServers;*/
+    int config[9]={2,2,2,1,1,1,0,1,0};          //replace this with whatever you get from the game, this is just for testing
+    cout << "Config looks like this: " << endl;
+    for(int i=0; i<9; i++){
+        cout << config[i] << " ";
+    }
+    //int demand[7];
+    int control[9];                     //this array will contain what we're giving back to the game to turn servers on/off
+    cout << "\n\nControl array: ";
+    for(int i=0; i<9; i++){				//starting at the beginning of the CONFIG array
+        //for(int j=4; j<=6; j++){		//ignore for now
+            if(config[i]<=1){
+                control[i]=1;
+            }
+            else{
+                control[i]=-1;
+            }
+        //}
+        cout << control[i] << " ";
+    }
 
-int revenue;
-int wcost;
-int jcost;
-int dcost;
-int numServers;
-int config[9];
-int demand[7];
-int control[9];
-string controlString;
-
-for(int i=0; i<=8; i++){				//starting at the beginning of the CONFIG array
-	//for(int j=4; j<=6; j++){			//starting at the first non date/time number of the DEMAND array (ignoring time for now)
-		numServers = config[i];			//sets the number of servers to the number in the jth position of the config array
-		if(numServers<2){
-			control[i]=1;
-		}
-		else
-			control[i]=-1;
-	//}
-}
-for(int i=0; i<=8; i++){
-	controlString=control[i]+" ";
-}
-for(int i=0; i<=8; i++){
-    cout<< controlString << endl;
-}
 }
