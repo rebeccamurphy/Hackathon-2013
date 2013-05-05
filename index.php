71c71a22d047a6b59f0a8d69b63c68b86fd08f60
John Paul Welsh <wearing2raincoats@yahoo.com>
2013-05-05 13:22:39 -0400

    allowing certain parameters to be variably set in index.php

--- index.php
+++ index.php
@@ -10,24 +10,33 @@ require ('gChart.php');
 <?php
 /**
  * This is a line graph intended to show the progression of profits across
- * the span of the game. Each turn (probably fewer) will be an entry into the
- * x-axis, and the corresponding revenue will be the y value.
+ * the span of the game. Every 10 minutes (20 turns) will be an entry into the
+ * x-axis, and the corresponding revenue at that time will be the y value.
  */
 $data_arr = array(); //will contain all the values we need to display, used in addDataSet
 array_push($data_arr, 1, 2, 3, 4, 5, 6, 7, 8, 9); //for now, initializes data_arr to have the numbers 1-9
+
+$x_axis_range_min = 0;    //first turn
+$x_axis_range_max = 3000; //last turn + 20 for good measure
+
+$y_axis_range_min = 20000;    //minimum value from data set
+$y_axis_range_max = -20000;   //maximum value from data set
+$y_axis_range_step = 1;       //we're doing every 10 mins, but the space between will be 1 (just labeled diff.)
 
 $lineChart = new gLineChart(300,300);
 $lineChart->addDataSet($data_arr);
 $lineChart->setLegend(array("Profits"));
 $lineChart->setColors(array("ff3344"));
 $lineChart->setVisibleAxes(array('x','y')); //I think the only valid values for this are 'x' or 'y'
-$lineChart->setDataRange(0,15); //I think this counts for both axes
-$lineChart->addAxisRange(0, 0, 10, 1); //this is for x axis -- PARAMETERS: axis index, start value, end value, count between steps on axis (opt.)
-$lineChart->addAxisRange(1, 0, 10, 1); //this is for y axis
+$lineChart->setDataRange($y_axis_range_min, $y_axis_range_max); //I think this counts for both axes
+//PARAMETERS below: axis index, start value, end value, count between steps on axis (opt.)
+$lineChart->addAxisRange(0, $x_axis_range_min, $x_axis_range_max, 1);
+$lineChart->addAxisRange(1, $y_axis_range_min, $y_axis_range_max, $y_axis_range_step);
 $lineChart->addAxisLabel(0, array("Turn"));
 $lineChart->addAxisLabel(1, array("Profits"));
-$lineChart->addBackgroundFill('bg', 'EFEFEF'); //PARAMETERS: part of chart being filled, color
-$lineChart->addBackgroundFill('c', '000000');
+//PARAMETERS below: part of chart being filled, color
+$lineChart->addBackgroundFill('bg', 'c4e53a');
+$lineChart->addBackgroundFill('c', 'FFFFFF');
 ?>
 <img src="<?php print $lineChart->getUrl();  ?>" /> <br> line chart using the gLineChart class.
 
